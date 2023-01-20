aeb2109182243533b5419efeaf3fb79ad8de5dbb
sol gudmin <115568958+schnappiiiiiiii@users.noreply.github.com>
2023-01-20 09:00:21 -0600

    removed original questions and added 8. forgot to commit after just one.

--- Country Flag Game/Data.swift
+++ Country Flag Game/Data.swift
@@ -8,31 +8,70 @@
 import Foundation
 struct Data {
     let questions = [
- 
+ //first 4
+        Question(correctAnswer:
+                Answer(text: "Angolia", isCorrect: true),
+              incorrectAnswers: [
+                Answer(text: "Austria", isCorrect: false),
+                Answer(text: "China", isCorrect: false),
+                Answer(text: "England", isCorrect: false)
+              ])
+        ,
+        Question(correctAnswer:
+                Answer(text: "Austria", isCorrect: true),
+              incorrectAnswers: [
+                Answer(text: "Angolia", isCorrect: false),
+                Answer(text: "China", isCorrect: false),
+                Answer(text: "England", isCorrect: false)
+              ])
+        ,
+        Question(correctAnswer:
+                Answer(text: "China", isCorrect: true),
+              incorrectAnswers: [
+                Answer(text: "Austria", isCorrect: false),
+                Answer(text: "Angolia", isCorrect: false),
+                Answer(text: "England", isCorrect: false)
+              ])
+        ,
         Question(correctAnswer:
                 Answer(text: "England", isCorrect: true),
               incorrectAnswers: [
-                Answer(text: "Ireland", isCorrect: false),
-                Answer(text: "Iceland", isCorrect: false),
-                Answer(text: "South Africa", isCorrect: false)
+                Answer(text: "Austria", isCorrect: false),
+                Answer(text: "China", isCorrect: false),
+                Answer(text: "Angollia", isCorrect: false)
               ])
         ,
- 
-        Question(correctAnswer:
-                Answer(text: "France", isCorrect: true),
-           incorrectAnswers: [
-                Answer(text: "Spain", isCorrect: false),
-                Answer(text: "Italy", isCorrect: false),
-                Answer(text: "Germany", isCorrect: false)
-           ])
-        ,
- 
-        Question(correctAnswer:
-                Answer(text: "Germany", isCorrect: true),
-           incorrectAnswers: [
-                Answer(text: "Austria", isCorrect: true),
-                Answer(text: "Italy", isCorrect: false),
-                Answer(text: "Czech Republic", isCorrect: false)
-           ])
-    ]
+    //second 4
+    Question(correctAnswer:
+            Answer(text: "Estonia", isCorrect: true),
+          incorrectAnswers: [
+            Answer(text: "France", isCorrect: false),
+            Answer(text: "Germany", isCorrect: false),
+            Answer(text: "Greece", isCorrect: false)
+          ])
+    ,
+    Question(correctAnswer:
+            Answer(text: "France", isCorrect: true),
+          incorrectAnswers: [
+            Answer(text: "Estonia", isCorrect: false),
+            Answer(text: "Germany", isCorrect: false),
+            Answer(text: "Greece", isCorrect: false)
+          ])
+    ,
+    Question(correctAnswer:
+            Answer(text: "Germany", isCorrect: true),
+          incorrectAnswers: [
+            Answer(text: "France", isCorrect: false),
+            Answer(text: "Estonia", isCorrect: false),
+            Answer(text: "Greece", isCorrect: false)
+          ])
+    ,
+    Question(correctAnswer:
+            Answer(text: "Greece", isCorrect: true),
+          incorrectAnswers: [
+            Answer(text: "France", isCorrect: false),
+            Answer(text: "Germany", isCorrect: false),
+            Answer(text: "Estonia", isCorrect: false)
+          ])
+    ,
 }
